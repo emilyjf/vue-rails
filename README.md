3464ef2dd20743612a2706fd22295456e84bc181
emilyjf <flanagan.emily.j@gmail.com>
2017-06-25 13:25:02 -0500

    show page

--- README.md
+++ README.md
@@ -22,3 +22,9 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+Exercise: Add more features to your Vue.js Todo app!
+1) Add the ability to filter items by All, Active, Completed
+2) Add a search box to filter items by a search input
+3) Add animations using the Vue.js Animations Cheatsheet
+BONUS: Use the HTML and CSS from the TodoMVC template to make your app look like the standard TodoMVC app! https://github.com/tastejs/todomvc-app-template
+https://github.com/tastejs/todomvc-app-css
--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -7,16 +7,43 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 {text: "groceries", completed: false}
               ],
               newTask: '',
-              errors: []
+              errors: [],
+              taskFilter: '',
+              sortAttribute: 'all, active, completed',
+              sortAscending: true
+
     },
     mounted: function(){
-      $.get('tasks.json', function(tasksResponse) {
+      $.get('/api/v1/tasks.json', function(tasksResponse) {
         this.tasks = tasksResponse;
       }.bind(this));
     },
+    computed: {
+      modifiedTasks: function() {
+        return this.tasks.sort(function(task1, task2) {
+          if (this.sortAscending) {
+            return task1[this.sortAttribute].localeCompare(task1[this.sortAttribute]);
+          } else {
+            return task2[this.sortAttribute].localeCompare(task2[this.sortAttribute]);
+          }
+        }.bind(this));
+      }
+    },
+    
     methods: {
+      setSortAttribute: function(inputAttribute) {
+        if (inputAttribute !== this.sortAttribute) {
+          this.sortAscending = true;
+        } else {
+          this.sortAscending = !this.sortAscending;
+        }
+        this.sortAttribute = inputAttribute;
+      },
+      isValidTask: function(inputTask) {
+        return inputTask.task.indexOf(this.taskFilter) !== -1
+       },
       toggleTask: function(task) {
-        task.completed = !task.completed;
+        task.taskVisible = !task.completed;
       },
       addTask: function() {
         this.errors = [];
@@ -26,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       $.post('api/v1/task.json', params, function (newTask){
         this.tasks.push(newTask);
         this.newTask = '';
-      }.bind(this)).fail(function(response) {
+      }.bind(this)).fail( function(response) {
         this.errors = response.responseJSON.errors;
       }.bind(this));
     },
